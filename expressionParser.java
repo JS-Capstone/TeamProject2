dd668bbcfc86f5649d479d3bc0f0173deca50b7d
olliepeel <ep260480@my.stchas.edu>
2024-07-01 21:35:35 -0500

    Set up main program file and project report

---
+++ expressionParser.java
@@ -0,0 +1,23 @@
+/*
+----------------------------------------------------------------------------------------------------------
+    Name:		main
+    Authors:	Ollie Peel, Jeffrey Saylor, Alex Brinkley, Rawan Alhachami
+    Language:	Java
+    Date:		2024-07-01
+    Purpose:	The purpose of this program is to use stacks to parse and evaluate expressions taken from
+    an input file and output the results to the console.
+----------------------------------------------------------------------------------------------------------
+    Change Log
+----------------------------------------------------------------------------------------------------------
+    Who		Date		Reason
+    OSP		2024-07-01	Original Version of Code
+----------------------------------------------------------------------------------------------------------
+*/
+
+public class expressionParser {
+    public static void main(String[] args) throws Exception{
+        // Read expressions from input file
+        // Use different user-defined classes to parse and evaluate expressions
+        // Output result to console
+    }
+}
